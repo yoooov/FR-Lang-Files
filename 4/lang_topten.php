997845eae1630427c89ad2c0ae7467b4f0e73c66
yoooov <poubelle.maison@gmail.com>
2016-11-06 11:45:54 +0100

    First commit

---
+++ 4/lang_topten.php
@@ -0,0 +1,58 @@
+<?php
+$lang = array(
+    //head
+    'head_title' => "Top 10",
+    //nav
+    'nav_torrents' => "Torrents",
+    'nav_countries' => "Pays",
+    'nav_peers' => "Peers",
+    //common
+    'common_rank' => "Rang",
+    'common_ratio' => "Ratio",
+    'common_infratio' => "Inf.",
+    'common_ul' => "Uploaded",
+    'common_users' => "Membres",
+    'common_top25' => "Top 25",
+    'common_top50' => "Top 50",
+    'common_top100' => "Top 100",
+    'common_top250' => "Top 250",
+    //user function
+    'user' => "User",
+    'user_ul' => "Uploadé",
+    'user_ulspeed' => "Vitesse UL",
+    'user_dl' => "Downloadé",
+    'user_dlspeed' => "Vitesse DL",
+    'user_joined' => "Rejoins le",
+    'user_topulers' => "Top %u Uploadeurs",
+    'user_topdlers' => "Top %u Downloadeurs",
+    'user_fastestup' => "Top %u des plus Rapides Uploadeurs <font class='small'>(average, includes inactive time)</font>",
+    'user_fastestdown' => "Top %u des plus Rapides Downloadeurs <font class='small'>(average, includes inactive time)</font>",
+    'user_bestshare' => "Top %u des Meilleurs Partageurs <font class='small'>(with minimum 1 GB downloaded)</font>",
+    'user_worstshare' => "Top %u des Pires Partageurs <font class='small'>(with minimum 1 GB downloaded)</font>",
+    //torrent function
+    'torrent_name' => "Name",
+    'torrent_snatch' => "Sna.",
+    'torrent_data' => "Data",
+    'torrent_seed' => "Se.",
+    'torrent_leech' => "Le.",
+    'torrent_total' => "To.",
+    'torrent_mostact' => "Top %u Most Active Torrents",
+    'torrent_mostsna' => "Top %u Most Snatched Torrents",
+    'torrent_datatrans' => "Top %u Most Data Transferred Torrents",
+    'torrent_bestseed' => "Top %u Best Seeded Torrents <font class='small'>(with minimum 5 seeders)</font>",
+    'torrent_worstseed' => "Top %u Worst Seeded Torrents <font class='small'>(with minimum 5 leechers, excluding unsnatched torrents)</font>",
+    //country function
+    'country' => "Country",
+    'country_mostact' => "Top %u Countries<font class='small'> (users)</font>",
+    'country_totalul' => "Top %u Countries<font class='small'> (total uploaded)</font>",
+    'country_avperuser' => "Top %u Countries<font class='small'> (average total uploaded per user, with minimum 1TB uploaded and 100 users)</font>",
+    'country_avg' => "Average",
+    'country_ratio' => "Top %u Countries<font class='small'> (ratio, with minimum 1TB uploaded, 1TB downloaded and 100 users)</font>",
+    //Peer function
+    'peers_uname' => "Username",
+    'peers_ulrate' => "Upload rate",
+    'peers_dlrate' => "Download rate",
+    'peers_fastestup' => "Top %u Fastest Uploaders",
+    'peers_fastestdown' => "Top %u Fastest Downloaders",
+);
+?>
