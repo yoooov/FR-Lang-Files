52be595a451a2e2326f7d1ce5f60f97a7b846a01
LATNEM <47476753+LATNEM@users.noreply.github.com>
2019-02-09 14:13:08 +0100

    Update lang_achievementbonus.php

--- 4/lang_achievementbonus.php
+++ 4/lang_achievementbonus.php
@@ -19,9 +19,9 @@
 $lang = array(
     'achbon_err' => 'Erreur',
     'achbon_off' => 'Il semble que la fonction "Trophées" soit désactivée.',
-    'achbon_std_head' => "Bonus Aléatoire",
-    'achbon_no_ach_bon_pnts' => "Aucun Points Bonus",
-    'achbon_no_ach_bon_pnts_msg' => "Il semble que vous n'ayez pour l'heure aucun Points Bonus à dépenser.",
+    'achbon_std_head' => "Bonus aléatoire",
+    'achbon_no_ach_bon_pnts' => "Aucun points bonus",
+    'achbon_no_ach_bon_pnts_msg' => "Il semble que vous n'ayez pour l'heure aucun points bonus à dépenser.",
     'achbon_congratulations' => "Félicitations",
     'achbon_you_hv_just_won' => "vous venez de gagner",
     'achbon_your_dl_been_reset_0' => "votre total téléchargé a été réinitialisé suite à une valeur inférieure à 0",
@@ -30,6 +30,6 @@ $lang = array(
     'achbon_failed_msg3' => "Le maléfique Adam a volé votre point de bonus.",
     'achbon_failed_msg4' => "Somehelp a utilisé votre bonus point afin d'acheter des croquettes pour remplir la gamelle de son chien.",
     'achbon_failed_msg5' => "Houdini a fait disparaître votre point de bonus, tentez votre chance la prochaine fois.",
-    'achbon_random_achievement_bonus' => "Bonus Aléatoire"
+    'achbon_random_achievement_bonus' => "Bonus aléatoire"
 );
 ?>
