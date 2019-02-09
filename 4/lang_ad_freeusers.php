ba9cc64c6ebe002e2493161d2fd6df809a678a0f
LATNEM <47476753+LATNEM@users.noreply.github.com>
2019-02-09 15:21:02 +0100

    Update lang_ad_freeusers.php

--- 4/lang_ad_freeusers.php
+++ 4/lang_ad_freeusers.php
@@ -18,25 +18,25 @@
  */
 $lang = array(
     'freeusers_wtf' =>  "WTF!",
-    'freeusers_msg' => "Le Freeleech sur tous les Torrents a été retiré par ",
+    'freeusers_msg' => "Le freeleech sur tous les torrents a été retiré par ",
     'freeusers_period'  =>  ".",
-    'freeusers_mod1'    =>  " - Freeleech sur tous les Torrents retiré par ",
-    'freeusers_msg_buffer'  =>  "Freeleech Notice!",
+    'freeusers_mod1'    =>  " - Freeleech sur tous les torrents retiré par ",
+    'freeusers_msg_buffer'  =>  "Annonce de freeleech!",
     'freeusers_log1' =>  "Compte utilisateur",
-    'freeusers_log2'    =>  "Le Freeleech sur tous les Torrents a été retiré par",
+    'freeusers_log2'    =>  "Le freeleech sur tous les torrents a été retiré par",
     'freeusers_fail'    =>  "Cet utilisateur a un statut No Freeleech!",
-    'freeusers_head'    =>  "Coup d'oeil au Freeleech",
+    'freeusers_head'    =>  "Coup d'oeil au freeleech",
     'freeusers_nothing' =>  "Rien ici",
     'freeusers_username'    =>  "Nom utilisateur",
     'freeusers_class'   =>  "Classe",
     'freeusers_expires' =>  "Expire",
-    'freeusers_remove'  =>  "Retiré le Freeleech",
+    'freeusers_remove'  =>  "Retiré le freeleech",
     'freeusers_notallowed'  =>  "Interdit",
     'freeusers_until'   =>  "Jusqu'à",
-    'freeusers_confirm' =>  "Etes-vous sûr de vouloir retirer le status Freeleech?",
+    'freeusers_confirm' =>  "Etes-vous sûr de vouloir retirer le status freeleech?",
     'freeusers_rem' =>  "Retirer",
     'freeusers_togo'    =>  "aller",
-    'freeusers_stdhead' =>  "Coup d'oeil au Freeleech"
+    'freeusers_stdhead' =>  "Coup d'oeil au freeleech"
  
    
 );
