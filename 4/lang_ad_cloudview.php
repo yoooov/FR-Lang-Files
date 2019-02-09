82c2868fd72172a18ebe02e3b6f54b9958ac5493
LATNEM <47476753+LATNEM@users.noreply.github.com>
2019-02-09 14:58:29 +0100

    Update lang_ad_cloudview.php

--- 4/lang_ad_cloudview.php
+++ 4/lang_ad_cloudview.php
@@ -19,15 +19,15 @@
 $lang = array(
 	'cloudview_success' => "Succès",
 	'cloudview_success_del' => "Les termes sélectionnés ont été supprimés avec succès!",
-	'cloudview_overview' => "Gestion de la Recherche Cloud",
+	'cloudview_overview' => "Gestion de la recherche Cloud",
 	'cloudview_phrase' => "Termes recherchés",
 	'cloudview_hits' => "Nombre de fois",
 	'cloudview_ip' => "IP",
 	'cloudview_del' => "Supprimer",
 	'cloudview_mark' => "Marquer",
-	'cloudview_markall_search' => "Cocher&nbsp;toutes&nbsp;les recherches",
-	'cloudview_markall' => "Tout Cocher",
+	'cloudview_markall_search' => "Cocher toutes les recherches",
+	'cloudview_markall' => "Tout cocher",
 	'cloudview_del_terms' => "Supprimer les termes choisis!",
-	'cloudview_stdhead' => "Vue du cloud"
+	'cloudview_stdhead' => "Vue du Cloud"
 );
 ?>
